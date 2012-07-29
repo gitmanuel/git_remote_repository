d157efab353f5f1768b4c89bb2c0ac636d9b08d8
Manuel <manuel@nautilus.com>
2012-07-29 13:09:43 -0300

    Starting from scratch.

--- README.md
+++
@@ -1,6 +0,0 @@
-git_remote_repository
-=====================
-
-This is the git remote repository on GitHub.
-
-This is the first commit.
--- SocialBoxing/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-SocialBoxing::Application.config.secret_token = 'ffdcafb216724242fb0bb5e7a446ae98e6ac49aa76658316c9be5ca59163d66ebcfb8e52f33078b292c428ef90212b1606a040490f92f3d503c617c44110a599'
